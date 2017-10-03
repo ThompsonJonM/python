4f78ed59c3d379a6b14aa700c9f7153de8c75c51
Jonathan Thompson <thompson.jonm@gmail.com>
2017-10-02 21:19:19 -0500

    complex functions

--- moreFunctions.py
+++ moreFunctions.py
@@ -7,8 +7,17 @@ def sum(*args):
         total+=arg
     return total
 
-def form_submission(**kwargs):
-    return 'Hello, {0}.'.format(kwargs['name'])
+# def form_submission(**kwargs):
+#     return 'Hello, {0}.'.format(kwargs['name'])
+
+def form_submission(**sub_data):
+    form_questions = ['name', 'email', 'address', 'age', 'gender']
+    for question in form_questions:
+        print('Respondent answered {1} for {0}'.format(question, sub_data.get(question, 'N/A')))
+    
+    # print(sub_data.get('age', 'N/A'))
+
+    return 'Hello, {0}'.format(sub_data['name'])
 
 print(form_submission(name='Carl', email='this@gmail.com', address='haha Lane'))
 
